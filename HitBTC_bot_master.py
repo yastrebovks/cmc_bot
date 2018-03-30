be8c570fe858f339a7421dfcd9525845c5019d3c
YastrebovKS <yastrebovks@gmail.com>
2018-03-30 22:51:52 +0300

    Big Changes
    Обновлен вывод комментариев: теперь в файл пишутся лишь события сделок, нет вывода результатов работы анализатора волн.
    Покупка берется по рынку.
    Исправление мелких багов.

--- HitBTC_bot_master.py
+++ HitBTC_bot_master.py
@@ -11,6 +11,7 @@ import os
 from datetime import datetime
 
 ### Глобальные флаги и переменные ###
+Working = True
 PROFIT = 0
 REFRESH = True
 USE_LOG = False
@@ -145,18 +146,13 @@ def get_ticks(market):
     chart_data = {}
     # Получаем данные свечей
     res = exchange.fetch_ohlcv(market, '5m')
-    print("OHLCV")
-    #print(res)
     # Заполнение массива для дальнейшего анализа
     for item in res:
         dt_obj = (datetime.fromtimestamp(item[0] / 1000))
-        #print(item[0])
-        #print(item)
         ts = int(time.mktime(dt_obj.timetuple()))
         if not ts in chart_data:
             chart_data[ts] = {'open': float(item[1]), 'close': float(item[4]), 'high': float(item[2]),
                               'low': float(item[3])}
-            #print(chart_data[ts]['close'])
 
     res = exchange.fetch_trades(market)
 
@@ -167,7 +163,6 @@ def get_ticks(market):
             dt_obj = datetime.strptime(trade['datetime'], '%Y-%m-%dT%H:%M:%SZ')
 
         ts = int((time.mktime(dt_obj.timetuple()) / 300)) * 300
-        # print(ts)
         if not ts in chart_data:
             chart_data[ts] = {'open': 0, 'close': 0, 'high': 0, 'low': 0}
 
@@ -182,7 +177,6 @@ def get_ticks(market):
         if not chart_data[ts]['low'] or chart_data[ts]['low'] > float(trade['price']):
             chart_data[ts]['low'] = float(trade['price'])
 
-    print("Close from Get_ticks!")
     return chart_data
 
 
@@ -217,8 +211,6 @@ def get_macd_advice(chart_data):
 def create_buy(market):
     global USE_LOG
     USE_LOG = True
-    log(market, 'Создаем ордер на покупку')
-    log(market, 'Получаем текущие курсы')
     # Берем цену лучшего аска
     current_rate = float(exchange.fetch_ticker(market)['ask'])
     # Проверяем возможность реинвестирования процентов
@@ -232,11 +224,7 @@ def create_buy(market):
         """ % (current_rate, CAN_SPEND, pair[1], can_buy, pair[0])
         )
     # Создание пробного ордера по заниженной цене
-    current_rate /= 10
-
-    print(current_rate)
-    #time.sleep(10)
-    order_res = exchange.create_order(market, 'limit', 'buy', can_buy, current_rate)
+    order_res = exchange.create_order(market, 'market', 'buy', can_buy, current_rate)
     # Заполняем БД при успешном создании оредера
     print(order_res)
     if order_res:
@@ -267,7 +255,7 @@ def create_buy(market):
                 'order_spent': CAN_SPEND
             })
         conn.commit()
-        log("Создан ордер на покупку %s" % order_res['info']['clientOrderId'])
+        log(order_res, " - Создан ордер на покупку")
     else:
         log(market, """
             Не удалось создать ордер: %s
@@ -341,7 +329,7 @@ def create_sell(from_order, market):
                 'from_order_id': from_order
             })
         conn.commit()
-        log(market, "Создан ордер на продажу %s" % order_res['info']['clientOrderId'])
+        log(order_res, " - Создан ордер на продажу")
     USE_LOG = False
 
 
@@ -351,11 +339,10 @@ exchange, MARKETS, CAN_SPEND, MARKUP, STOCK_FEE, ORDER_LIFE_TIME, keys, REFRESH 
 
 # Основная логика, бесконечный цикл
 
-while True:
+while Working:
     try:
         # Проходим по каждой торгуемой паре из списка в начале
         for market in MARKETS:
-            log(market, "Получаем все неисполненные ордера по БД")
             orders_q = """
                        SELECT
                          o.order_id,
@@ -388,23 +375,17 @@ while True:
                                             'order_amount': row[3], 'order_filled': row[4], 'order_created': row[5],
                                             'partially_filled': False, 'order_cancelled': False
                                             }
-                print(row[4])
             if orders_info:
                 # Проверяем, были ли выполнены ранее созданные ордера, и помечаем в БД.
-                log(market, "Получены неисполненные ордера из БД", orders_info)
                 for order in orders_info:
                     if not orders_info[order]['order_filled']:
-                        log(market, "Проверяем состояние ордера %s" % order)
                         # Запрашиваем данные ордера у биржи
                         order_info = exchange.fetch_order(orders_info[order]['order_id'])
-                        print(order_info)
                         # Проверяем пришедший статус
                         if order_info['status'] == 'closed':
-                            log(market, 'Ордер %s уже выполнен!' % order)
-
                             if(order_info['fee'] == None):
                                 print("FEEEEE")
-                                order_info['fee'] = 0
+                                order_info['fee'] = STOCK_FEE
                             cursor.execute(
                                 """
                                   UPDATE orders
@@ -424,10 +405,9 @@ while True:
                                 }
                             )
                             conn.commit()
-                            log(market, "Ордер %s помечен выполненным в БД" % order)
+                            log(orders_info[order], " - Ордер помечен выполненным в БД")
                             orders_info[order]['order_filled'] = datetime.now()
                         elif order_info['status'] == 'canceled':
-                            log(market, 'Ордер %s отменен!' % order)
                             cursor.execute(
                                 """
                                   UPDATE orders
@@ -447,12 +427,10 @@ while True:
                                 }
                             )
                             conn.commit()
-                            log(market, "Ордер %s помечен отмененным в БД" % order)
+                            log(orders_info[order], " - Ордер помечен отмененным в БД")
                             orders_info[order]['order_cancelled'] = datetime.now()
-
                         else:
                             orders_info[order]['order_cancelled'] = False
-                            log(market, "Ордер %s еще не выполнен" % order)
                             # Проверяем на частичное выполнение ордера
                             if order_info['remaining'] != order_info['amount']:
                                 orders_info[order]['partially_filled'] = True
@@ -465,8 +443,7 @@ while True:
                                     chart_data=get_ticks(market))  # проверяем, можно ли создать sell
                                 if macd_advice['trand'] == 'BEAR' or (
                                         macd_advice['trand'] == 'BULL' and macd_advice['growing']):
-                                    log(market,
-                                        'Для ордера %s не создаем ордер на продажу, т.к. ситуация на рынке неподходящая' % order)
+                                        print('Для ордера не создаем ордер на продажу, т.к. ситуация на рынке неподходящая')
                                 else:
                                     log(market, "Для выполненного ордера на покупку выставляем ордер на продажу")
                                     create_sell(from_order=orders_info[order]['order_id'], market=market)
@@ -482,9 +459,8 @@ while True:
                                 time_passed = int((datetime.utcnow() - datetime(1970, 1, 1)).total_seconds()) - int(time.mktime(datetime.strptime(orders_info[order]['order_created'],
                                                       "%Y-%m-%d %H:%M:%S").timetuple()))
                                 if time_passed-10800 > ORDER_LIFE_TIME * 60:
-                                    log('Пора отменять ордер %s' % order)
                                     cancel_res = exchange.cancel_order(order)
-                                    if cancel_res['success']:
+                                    if cancel_res['status'] == 'canceled':
                                         cursor.execute(
                                             """
                                               UPDATE orders
@@ -497,23 +473,19 @@ while True:
                                             }
                                         )
                                         conn.commit()
-                                        log(market, "Ордер %s помечен отмененным в БД" % order)
+                                        log(orders_info[order], " - Ордер помечен отмененным в БД")
                     else:  # Ордер на продажу
                         pass
             else:
-                log(market, "Неисполненных ордеров в БД нет, пора ли создать новый?")
                 # Проверяем MACD, если рынок в нужном состоянии, выставляем ордер на покупку
                 if USE_MACD:
                     macd_advice = get_macd_advice(chart_data=get_ticks(market))
                     # Условия для покупки: BEAR и растущий прогноз MACD
                     if macd_advice['trand'] == 'BEAR' and macd_advice['growing']:
-                        log(market, "Создаем ордер на покупку1")
                         create_buy(market=market)
-                    else:
-                        log(market, "Условия рынка не подходят для торговли", macd_advice)
                 else:
-                    log(market, "Создаем ордер на покупку2")
                     create_buy(market=market)
         time.sleep(1)
     except Exception as e:
+        print("Exit bot")
         print(e)
